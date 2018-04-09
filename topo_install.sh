f90f89de9da61dd6085c8d280eecc37c247d6e75
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-09 12:36:56 +0200

    Update topo_install.sh

--- topo_install.sh
+++ topo_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 1.-Descarga
-sudo git clone https://github.com/OCA/l10n-spain.git -b 8.0 l10n-spain/opt/odoo/l10n-spain
+sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain
 
 #.-Acceso directo
 
