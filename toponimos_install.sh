30069c4add6e0e0833cc01bd455c988db39255f4
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-09 13:02:54 +0200

    Update toponimos_install.sh

--- toponimos_install.sh
+++ toponimos_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # 1.-Descarga
 sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain
-
+sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 opt/odoo/partner-contact
 #.-Acceso directo
 
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_toponyms /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/partner-contact/base_continent /opt/custom/addons
+sudo ln -s /opt/odoo/partner-contact/base_location_geonames_import /opt/odoo/custom/addns
