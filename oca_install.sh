3eef271af73c9a44669c45d6837288273cbaf290
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-10 12:07:02 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -3,3 +3,32 @@
 sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain
 sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/partner-contact
 #.-Acceso directo
+
+
+account_export_csv,
+account_move_line_report_xls,
+account_chart_update ,
+account_invoice_constraint_chronology,
+account_invoice_currency, 
+account_journal_always_check_date,
+account_renumber,
+account_due_list,
+account_due_list_payment_mode,
+account_banking_mandate, 
+account_banking_pain_base,
+account_banking_payment_export,
+account_banking_payment_transfer,
+account_banking_sepa_direct_debit,
+account_direct_debit,
+account_payment_partner,
+account_bank_statement_import,
+attachment_preview,
+attachments_to_filesystem,
+document_page,
+document_url,
+account_balance_reporting_xls,
+account_balance_reporting,
+account_refund_original,
+l10n_es_account_asset,
+l10n_es_account_balance_report,
+l10n_es_account_bank_statement_import_n43,
