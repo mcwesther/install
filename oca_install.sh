d33ced792d02ff9e281bd31cec91314c81baed88
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-10 12:05:10 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -1,3 +1,5 @@
-#bin bach
-crear
-https://github.com/OCA/l10n-spain.git
+#!/bin/bash
+# 1.-Descarga
+sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain
+sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/partner-contact
+#.-Acceso directo
