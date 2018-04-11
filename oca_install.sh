78f9dfcb7dfad501789b11e32231eacd85678800
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-11 09:50:23 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -2,7 +2,7 @@
 # 1.-Descarga
 sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain)
 sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/partner-contact)
-https://github.com/OCA/account-financial-reporting.git
+sudo git clone https://github.com/OCA/account-financial-reporting.git
 #.-Acceso directo
 
 
