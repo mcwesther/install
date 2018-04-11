83e654f7e1e1bdbe19aee442c741845df368e6d5
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-11 09:37:32 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -5,30 +5,30 @@ sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/part
 #.-Acceso directo
 
 
-account_export_csv,
-account_move_line_report_xls,
+account_export_csv,/opt/odoo/custom/addons
+account_move_line_report_xls,/opt/odoo/custom/addons
 account_chart_update ,
-account_invoice_constraint_chronology,
+account_invoice_constraint_chronology,/opt/odoo/custom/addons
 account_invoice_currency, 
-account_journal_always_check_date,
-account_renumber,
-account_due_list,
-account_due_list_payment_mode,
-account_banking_mandate, 
-account_banking_pain_base,
-account_banking_payment_export,
-account_banking_payment_transfer,
-account_banking_sepa_direct_debit,
-account_direct_debit,
-account_payment_partner,
-account_bank_statement_import,
-attachment_preview,
-attachments_to_filesystem,
-document_page,
-document_url,
-account_balance_reporting_xls,
-account_balance_reporting,
-account_refund_original,
-l10n_es_account_asset,
-l10n_es_account_balance_report,
-l10n_es_account_bank_statement_import_n43,
+account_journal_always_check_date,/opt/odoo/custom/addons
+account_renumber,/opt/odoo/custom/addons
+account_due_list,/opt/odoo/custom/addons
+account_due_list_payment_mode,/opt/odoo/custom/addons
+account_banking_mandate,/opt/odoo/custom/addons 
+account_banking_pain_base,/opt/odoo/custom/addons
+account_banking_payment_export,/opt/odoo/custom/addons
+account_banking_payment_transfer,/opt/odoo/custom/addons
+account_banking_sepa_direct_debit,/opt/odoo/custom/addons
+account_direct_debit,/opt/odoo/custom/addons
+account_payment_partner,/opt/odoo/custom/addons
+account_bank_statement_import,/opt/odoo/custom/addons
+attachment_preview,/opt/odoo/custom/addons
+attachments_to_filesystem,/opt/odoo/custom/addons
+document_page,/opt/odoo/custom/addons
+document_url,/opt/odoo/custom/addons
+account_balance_reporting_xls,/opt/odoo/custom/addons
+account_balance_reporting,/opt/odoo/custom/addons
+account_refund_original,/opt/odoo/custom/addons
+l10n_es_account_asset,/opt/odoo/custom/addons
+l10n_es_account_balance_report,/opt/odoo/custom/addons
+l10n_es_account_bank_statement_import_n43,/opt/odoo/custom/addons
