3e8d6e213be5da6e5292eed10b5dbcfde5014494
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-11 10:36:12 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -3,6 +3,8 @@
 
 
 sudo git clone https://github.com/OCA/account-financial-reporting.git -b 10.0 /opt/odoo/account-financial-reporting
+
+sudo git clone https://github.com/OCA/account-financial-tools.git -b 10.0/opt/odoo/account-financial-tools
 #.-Acceso directo
 
 
@@ -10,12 +12,12 @@ sudo git clone https://github.com/OCA/account-financial-reporting.git -b 10.0 /o
 
 sudo ln -s /opt/odoo/account-financial-reporting/account_export_csv /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-reporting/account_move_line_report_xls /opt/odoo/custom/addons
-sudo ln -s /opt/odoo/account_chart_update ,
-sudo ln -s /opt/odoo/account_invoice_constraint_chronology,/opt/odoo/custom/addons
-sudo ln -s /opt/odoo/account_invoice_currency, 
-sudo ln -s /opt/odoo/account_journal_always_check_date,/opt/odoo/custom/addons
-sudo ln -s /opt/odoo/account_renumber,/opt/odoo/custom/addons
-sudo ln -s /opt/odoo/account_due_list,/opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_chart_update//opt/odoo/custom/addons              
+sudo ln -s /opt/odoo/account-financial-tools/account_invoice_constraint_chronology /opt/odoo/custom/addons                    
+sudo ln -s /opt/odoo/account-financial-tools/account_invoice_currency /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account_journal_always_check_date /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_renumber /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account_due_list,/opt/odoo/custom/addons????
 sudo ln -s /opt/odoo/account_due_list_payment_mode,/opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account_banking_mandate,/opt/odoo/custom/addons 
 sudo ln -s /opt/odoo/account_banking_pain_base,/opt/odoo/custom/addons
