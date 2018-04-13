bd44e4f0f42ae1b3d24b2263daf3558b450ae91d
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-13 10:25:34 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -18,7 +18,7 @@ sudo ln -s /opt/odoo/account-financial-reporting/account_move_line_report_xls /o
 sudo ln -s /opt/odoo/account-financial-tools/account_chart_update /opt/odoo/custom/addons              
 sudo ln -s /opt/odoo/account-financial-tools/account_invoice_constraint_chronology /opt/odoo/custom/addons                    
 sudo ln -s /opt/odoo/account-financial-tools/account_invoice_currency /opt/odoo/custom/addons
-sudo ln -s /opt/odoo/account_journal_always_check_date /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_journal_always_check_date /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-tools/account_renumber /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-payment/account_due_list /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-payment/account_due_list_payment_mode /opt/odoo/custom/addons
