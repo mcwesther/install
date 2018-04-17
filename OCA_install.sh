a05d7e303daa51ef3f125ae12101df9141ff110f
mcwesther <38205502+mcwesther@users.noreply.github.com>
2018-04-17 12:22:39 +0200

    Update OCA_install.sh

--- OCA_install.sh
+++ OCA_install.sh
@@ -9,7 +9,7 @@ sudo git clone https://github.com/OCA/knowledge.git -b 8.0 /opt/odoo/knowledge.g
 sudo git clone https://github.com/OCA/l10n-spain.git -b 8.0 /opt/odoo/l10n-spain.git
 sudo git clone https://github.com/OCA/partner-contact.git -b 8.0 /opt/odoo/partner-contact
 sudo git clone https://github.com/OCA/reporting-engine.git -b 8.0 /opt/odoo/reporting-engine
-sudo git clone
+sudo git clone https://github.com/OCA/server-tools.git -b 8.0 /opt odoo/server-tools
 
 #.-Acceso directo
 sudo ln -s /opt/odoo/account-financial-reporting/account_export_csv /opt/odoo/custom/addons
@@ -60,7 +60,7 @@ sudo ln -s /opt/odoo/partner-contact/base_location_geonames_import /opt/odoo/cus
 sudo ln -s /opt/odoo/partner-contact/base_partner_sequence /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/reporting-engine/report_custom_filename /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/reporting-engine/report_xls /opt/odoo/custom/addons
-sudo ln -s /opt/odoo//disable_openerp_online,/opt/odoo/custom/addons
+sudo ln -s /opt/odoo/server-tools/disable_openerp_online,/opt/odoo/custom/addons
 sudo ln -s /opt/odoo//mass_editing,/opt/odoo/custom/addons
 sudo ln -s /opt/odoo//web_export_view,/opt/odoo/custom/addons
 
